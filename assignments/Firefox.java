1e0372e7a6f15e8eafc13dc2b0e88a007ecdd558
AbinayaArun <137876027+AbinayaArun@users.noreply.github.com>
2023-07-17 18:23:47 +0530

    Add files via upload
    Day1_17/07_Datatypes

---
+++ assignments/Firefox.java
@@ -0,0 +1,38 @@
+package assignments;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* 17 july Assigment on datatypes */
+		
+		//1) version = 100.2  (use dataType float) 
+		float firefoxversion= 100.2f;
+		System.out.println("The version of Browser is :" + firefoxversion);	
+		
+		
+		//2) name = "firefox" (use dataType String)
+		String browsername= "firefox";
+		System.out.println("The Name of Browser is :" + browsername);
+		
+		
+		//3) visible = true (use dataType boolean)
+		boolean Isvisible =true;
+		System.out.println("Browser visibility to users ?" + Isvisible);
+		
+		
+		//4) year = 1998 (use dataType int)
+		int releasedyear =1998;
+		System.out.println("The browser relased on :" + releasedyear);
+		
+		
+		//5) logo = 'm' (Use datatype char)
+		char logo= 'm';
+		System.out.println("The logo of the browser :" + logo);
+				
+	
+		
+	}
+
+}
